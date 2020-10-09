3981ce42985008d10497ed2127f0968d697f0f02
David Palao <david.palao@gmail.com>
2020-10-09 11:30:11 +0200

    test: first failing UT for serializers

---
+++ jogging/tests/test_serializers.py
@@ -0,0 +1,39 @@
+########################################################################
+#
+#  Copyright (c) 2020 David Palao
+#
+#  This file is part of JoggingStats.
+#
+#  JoggingStats is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  JoggingStats is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with JoggingStats. If not, see <http://www.gnu.org/licenses/>.
+#
+########################################################################
+
+#import unittest
+
+from django.contrib.auth.models import User
+from django.test import TestCase
+from rest_framework.test import APIRequestFactory
+
+
+class NewAccountSerializerTestCase(TestCase):
+    def test_can_create_account(self):
+        factory = APIRequestFactory()
+        request = factory.post(
+            "/new-user/",
+            {"username": "mike", "password": "1259f"},
+            format="json",
+        )
+        self.assertEqual(User.objects.count(), 1)
+        user = User.objects.first()
+        self.assertEqual(user.username, "mike")
