4c68a50255ccb76584c69f613c12a1750eab43f5
David Palao <david.palao@gmail.com>
2020-10-12 12:32:16 +0200

    test: FT to ensure that users can only access their own data

--- functional_tests/test_regular_user.py
+++ functional_tests/test_regular_user.py
@@ -25,30 +25,75 @@ import json
 from django.contrib.staticfiles.testing import LiveServerTestCase
 import requests
 
+TODAY = date.today()
 
 class RegularUserTestCase(LiveServerTestCase):
     username = "bob"
     password = "1Mpossibl3"
-
+    run_data = [
+        {
+            "date": str(TODAY),
+            "distance": 11.3,
+            "time": str(timedelta(minutes=58, seconds=24)),
+            "location": "Frankfurt am Main",
+        },
+        {
+            "date": str(TODAY-timedelta(days=1)),
+            "distance": 15.9,
+            "time": str(timedelta(hours=1, minutes=22, seconds=47)),
+            "location": "Frankfurt am Main",
+        },
+    ]
+    another_username = "mike"
+    another_password = "ju=988X"
+    another_run_data = [
+        {
+            "date": str(TODAY-timedelta(days=10)),
+            "distance": 5.3,
+            "time": str(timedelta(minutes=25, seconds=4)),
+            "location": "Madrid",
+        },
+        {
+            "date": str(TODAY-timedelta(days=8)),
+            "distance": 5.3,
+            "time": str(timedelta(minutes=25, seconds=55)),
+            "location": "Madrid",
+        },            
+    ]
+    
     def setUp(self):
-        ## all the tests in this class will require authentication:
-        self.auth_data = {"username": self.username, "password": self.password}
+        # another user has already created an account:
+        auth_data = {
+            "username": self.another_username, "password": self.another_password
+        }
         post_resp = requests.post(
-            self.live_server_url+"/new-account/", data=self.auth_data
+            self.live_server_url+"/new-account/", data=auth_data
         )
-
+        # and posted some data:
+        for item in self.another_run_data:
+            requests.post(
+                self.live_server_url+"/run/",
+                data=item, auth=(auth_data["username"], auth_data["password"])
+            )
+        
+        # Bob himself made an account too:
+        auth_data = {"username": self.username, "password": self.password}
+        post_resp = requests.post(
+            self.live_server_url+"/new-account/", data=auth_data
+        )
+        self.auth_data = (self.username, self.password)
+        
     def check_post(self, resp, data):
-        # the response is ok
-        self.assertTrue(resp.ok)
-        # with the proper status code:
+        # he gets the proper status code:
         self.assertEqual(resp.status_code, 201)
         # and the reason is: the run data was created
         self.assertEqual(resp.reason, "Created")
         # and to wrap it up, the text of the response has the posted data:
-        expected_data = {
-            k: data[k] for k in ("date", "distance", "time", "location")
-        }
-        self.assertEqual(resp.text, json.dumps(expected_data))
+        resp_data = json.loads(resp.content)
+        # to handle properly leading zeros:
+        h, m, s = [float(_) for _ in resp_data["time"].split(":")]
+        resp_data["time"] = str(timedelta(hours=h, minutes=m, seconds=s))
+        self.assertEqual(resp_data, data)
 
     def check_get_run(self, resp, items):
         # the response is ok
@@ -58,51 +103,51 @@ class RegularUserTestCase(LiveServerTestCase):
         # and the reason is expectedly OK
         self.assertEqual(resp.reason, "OK")
         # Now, he sees in the response the data previously posted:
-        posted_runs = json.loads(resp.text)
-        results = posted_runs["results"]
-        count = posted_runs["count"]
-        self.assertEqual(count, len(items))
+        posted_runs = json.loads(resp.content)
+        results = []
+        for run in posted_runs:
+            # to handle properly leading zeros:
+            h, m, s = [float(_) for _ in run["time"].split(":")]
+            run["time"] = str(timedelta(hours=h, minutes=m, seconds=s))
+            results.append(run)
         for item in items:
             self.assertIn(item, results)
 
     def test_can_save_data(self):
         # Now that he has an account, Bob would like to finally send
         # data for his run this morning:
-        today = date.today()
-        runtime = timedelta(minutes=58, seconds=24)
-        todays_stats = {
-            "date": str(today),
-            "distance": 11.3,
-            "time": runtime,
-            "location": "Frankfurt am Main",
-        }
-        full_data = self.auth_data.copy()
-        full_data.update(todays_stats)
         post_resp = requests.post(
-            self.live_server_url+"/run", data=full_data
+            self.live_server_url+"/run/", data=self.run_data[0], auth=self.auth_data
         )
-        self.check_post(post_resp, full_data)
+        self.check_post(post_resp, self.run_data[0])
         # Wonderful!
         # He also stores yesterday's run data:
-        yesterday = today - timdedelta(days=1)
-        yesterdays_stats = {
-            "date": str(yesterday),
-            "distance": 15.9,
-            "time": timedelta(hours=1, minutes=22, seconds=47),
-            "location": "Frankfurt am Main",
-        }
-        full_data.update(yesterdays_stats)
         post_resp = requests.post(
-            self.live_server_url+"/run", data=full_data
+            self.live_server_url+"/run/",
+            data=self.run_data[1], auth=self.auth_data
         )
-        self.check_post(post_resp, full_data)
+        self.check_post(post_resp, self.run_data[1])
         # But he is wondering... are the data really stored? He tries to
         # retrieve them:
         get_resp = requests.get(
-            self.live_server_url+"/run", data=self.auth_data
+            self.live_server_url+"/run/", auth=self.auth_data
         )        
         # and indeed he can retrieve the uploaded data!
-        self.check_get_run(get_resp, [todays_stats, yesterdays_stats])
+        self.check_get_run(get_resp, self.run_data)
         # That makes him very happy: he can use that functionality to
         # analyze his progress.
 
+    def test_can_see_only_own_data(self):
+        # after posting some data:
+        for run_data in self.run_data:
+            post_resp = requests.post(
+                self.live_server_url+"/run/", data=run_data, auth=self.auth_data
+            )
+        # and out of curiosity, Bob wonders if he can sees his cousin's records.
+        # But he checks that he can only see his own data:
+        get_resp = requests.get(
+            self.live_server_url+"/run/", auth=self.auth_data
+        )
+        retrieved_runs = json.loads(get_resp.content)
+        self.assertEqual(len(retrieved_runs), len(self.run_data))
+        # he knows that because he knows how many runs he uploaded.
