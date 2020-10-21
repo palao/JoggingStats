a26299174682e86d4956b685e20684d89a1a4764
David Palao <david.palao@gmail.com>
2020-10-21 15:49:17 +0200

    test: interface defined in search module (as placeholder for now)

---
+++ jogging/search.py
@@ -0,0 +1,22 @@
+########################################################################
+#
+#  Copyright (c) 2020 David Palao
+#
+#  This file is part of JoggingStats.
+#
+#  JoggingStats is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  JoggingStats is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with JoggingStats. If not, see <http://www.gnu.org/licenses/>.
+#
+########################################################################
+
+make_Qexpr_from_search_string = None
